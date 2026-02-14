67422df44bf32b3896969768b87ab849df1a93fc
Aniruddha <aniruddha.abk@gmail.com>
2026-02-15 01:13:59 +0530

    dynamic styling added based on todo status

--- src/app/components/todo/todo.ts
+++ src/app/components/todo/todo.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoItemModel } from '../../models/todo.model';
 import { Todoservice } from '../../services/todoservice';
+import { NgClass } from '@angular/common';
+import { TodoStatus } from '../../models/todo-status.enum';
 
 @Component({
   selector: 'app-todo',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './todo.html',
   styleUrl: './todo.css',
 })
 export class Todo implements OnInit {
   todoList: TodoItemModel[] = [];
   newTask: string = "";
+  todoStatus = TodoStatus;
 
   constructor(private todoService: Todoservice) { }
 
